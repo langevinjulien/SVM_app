b602460e91c5a9ad2a17d2226155dc9579a3a366
langevinjulien <54897903+langevinjulien@users.noreply.github.com>
2020-08-01 15:54:16 +0200

    Ajout des graphs

--- app.R
+++ app.R
@@ -95,6 +95,33 @@ ui <- shinyUI(
     .left-side, .main-sidebar {
         padding-top: 175px;
     }"
+            ),
+            
+            tabItems(
+                # Onglet Principe du SVM
+                tabItem(tabName = "principe",
+                        fluidRow(
+                            box(width = 12, 
+                                title = "Echantillon linéairement séparable",
+                                plotOutput("plot_svm")
+                            ) 
+                        ),
+                        
+                        fluidRow(
+                            box(title = "Echantillon presque linéairement séparable",
+                                plotOutput("plot_svm_2")
+                            )  
+                        ),
+                        
+                        fluidRow(
+                            box(width = 12,
+                                title = "Echantillon non linéairement séparable",
+                                uiOutput("plot_kt"),
+                                actionButton("action_kernel_trick", "Changer la dimension"),
+                                radioButtons("choix","Choisir dimension",choices=c("2d"="2d","3d"="3d"))
+                            )  
+                        )
+                )
             )
         )
     )
@@ -102,15 +129,183 @@ ui <- shinyUI(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+## SetUp
+    
+    ## Valeurs reactives
+    values <- reactiveValues(plot_dimensions = NULL,
+                             kernel_trick_index = 1)
+    
+    ## Fonctions
+    # Determiner si un nombre est pair ou non
+    is.even <- function(x){ x %% 2 == 0 }
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+## Onglet Présentation ########################################################################################################
+## Onglet Principe du SVM #####################################################################################################
+    
+    ## Cas linéairement séparable
+    plot_svm <- reactive ({
+        set.seed(585)
+        n = 25
+        a1 = rnorm(n)
+        a2 = 1 - a1 + 2* runif(n)
+        b1 = rnorm(n)
+        b2 = -1 - b1 - 2*runif(n)
+        x = rbind(matrix(cbind(a1,a2),,2),matrix(cbind(b1,b2),,2))
+        y <- matrix(c(rep(1,n),rep(-1,n)))
+        plot(x,col=ifelse(y>0,2,3),pch=".",cex=8)
+        
+        dat = data.frame(x, y = as.factor(y)) # Transforme les données en fataframe
+        svmfit = svm(y ~ ., data = dat, kernel = "linear", cost = 50, scale = FALSE)
+        # print(svmfit)
+        # 
+        # plot(svmfit, dat)
+        
+        ## Représentation graphique du SVM
+        make.grid <- function(x, n=75) {
+            grange = apply(x, 2, range) # récupère les plus petites et les plus grandes valeurs de toutes les variables
+            x1 = seq(from = grange[1,1], to = grange[2,1], length = n)
+            x2 = seq(from = grange[1,2], to = grange[2,2], length = n)
+            expand.grid(X1=x1, X2=x2)
+        }
+        
+        xgrid <- make.grid(x)
+        xgrid[1:10,]
+        
+        ygrid = predict(svmfit, xgrid)
+        # plot(xgrid, col = c("red","blue")[as.numeric(ygrid)], pch = 20, cex = .2)
+        # points(x, col = y + 3, bg = y + 3, pch = 22)
+        # points(x[svmfit$index,], pch = 1, cex = 4)
+        
+        beta = drop(t(svmfit$coefs)%*%x[svmfit$index,])
+        beta0 = svmfit$rho
+        
+        plot(xgrid, col = c(rgb(red = 1, green = 0, blue = 0, alpha = 0.4),
+                            rgb(red = 0, green = 0, blue = 1, alpha = 0.4))[as.numeric(ygrid)], pch = 20, cex = 0.01)
+        points(x, col = y + 3, pch = 19, cex = 1)
+        points(x[svmfit$index,], pch = 5, cex = 2)
+        abline(beta0 / beta[2], -beta[1] / beta[2])
+        abline((beta0 - 1) / beta[2], -beta[1] / beta[2], lty = 2)
+        abline((beta0 + 1) / beta[2], -beta[1] / beta[2], lty=2)
     })
+    
+    output$plot_svm <- renderPlot(plot_svm())
+    
+    
+    ## Cas non linéairement séparable
+    plot_svm_2 <- reactive({
+        set.seed(585)
+        n = 25
+        a1 = rnorm(n)
+        a2 = 1 - a1 + 2* runif(n)
+        b1 = rnorm(n)
+        b2 = -1 - b1 - 2*runif(n)
+        x = rbind(matrix(cbind(a1,a2),,2),matrix(cbind(b1,b2),,2))
+        y <- matrix(c(rep(1,n),rep(-1,n)))
+        y[32] <- 1
+        plot(x,col=ifelse(y>0,2,3),pch=".",cex=8)
+        
+        dat = data.frame(x, y = as.factor(y)) # Transforme les données en fataframe
+        svmfit = svm(y ~ ., data = dat, kernel = "linear", cost = 50, scale = FALSE)
+        
+        ## Représentation graphique du SVM
+        make.grid <- function(x, n=75) {
+            grange = apply(x, 2, range) # récupère les plus petites et les plus grandes valeurs de toutes les variables
+            x1 = seq(from = grange[1,1], to = grange[2,1], length = n)
+            x2 = seq(from = grange[1,2], to = grange[2,2], length = n)
+            expand.grid(X1=x1, X2=x2)
+        }
+        
+        xgrid <- make.grid(x)
+        xgrid[1:10,]
+        
+        ygrid = predict(svmfit, xgrid)
+        
+        beta = drop(t(svmfit$coefs)%*%x[svmfit$index,])
+        beta0 = svmfit$rho
+        
+        plot(xgrid, col = c(rgb(red = 1, green = 0, blue = 0, alpha = 0.4),
+                            rgb(red = 0, green = 0, blue = 1, alpha = 0.4))[as.numeric(ygrid)], pch = 20, cex = 0.2)
+        points(x, col = y + 3, pch = 19)
+        points(x[svmfit$index,], pch = 5, cex = 2)
+        abline(beta0 / beta[2], -beta[1] / beta[2])
+        abline((beta0 - 1) / beta[2], -beta[1] / beta[2], lty = 2)
+        abline((beta0 + 1) / beta[2], -beta[1] / beta[2], lty=2)
+    })
+    
+    
+    output$plot_svm_2 <- renderPlot(plot_svm_2())
+    
+    ## Kernel Trick
+    
+    # Dimension du graph
+    observeEvent(input$action_kernel_trick, {
+
+        values$kernel_trick_index <- values$kernel_trick_index +1
+        
+        if (is.even(values$kernel_trick_index)) {
+            values$plot_dimensions <- "2d"
+        } else {
+            values$plot_dimensions <- "3d"
+        }
+    })
+    
+    # Graphique en 2d et 3d
+    output$plot_kt <- renderUI ({
+        
+        theta <- sample(1:360, 70, replace=TRUE)
+        
+        # Points bleus
+        x1 <- sample(8:10, 70, replace=TRUE)*cos(theta)
+        x2 <- sample(8:10, 70, replace=TRUE)*sin(theta)
+        x3 <- runif(70, min = 2, max = 4)
+        
+        # Points rouges
+        x11 <- sample(1:3, 70, replace=TRUE)*cos(theta)
+        x22 <- sample(1:3, 70, replace=TRUE)*sin(theta)
+        x33 <- runif(70, min = 6, max = 8)
+        
+        df <- data.frame(x=c(x1,x11), y=c(x2,x22), z=c(x3,x33))
+        
+        a=ifelse(abs(df$x)>5 | abs(df$y)>5,1,-1)
+        a
+        
+
+        if (input$choix == "3d") {
+            
+            output$plot_kernel_trick <- renderPlot({
+                try(rgl.close())
+                plot(df[,1:2], col = a + 3, pch = 19)
+            })
+            
+            p <- plotOutput("plot_kernel_trick")
+           
+        } else {
+            
+            output$plot_kernel_trick <- renderRglwidget({
+                plot3d(df$x, df$y, df$z, col = a + 3, size = 2, type='s')
+                rglwidget()
+            })
+            
+            p <- rglwidgetOutput("plot_kernel_trick")
+        }
+        
+        browser()
+        test <- p
+        
+        return(p)
+    })
+    
+    # Output du Graphique sur le Kernel Trick
+    # output$plot_kernel_trick <- renderRglwidget(plot_kernel_trick())
+    
+    
+    
+    
+    # Graphique à 2 dimensions
+    # Graphique à 3 dimensions
+    
+## Onglet Comparaisons ########################################################################################################
 }
 
 # Run the application 
